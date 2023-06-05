d3e973b0e9f40012f964c882ff0022ef60ab3d82
abdelkader <bouzomitaabdelkader@gmail.com>
2023-06-05 13:30:28 +0100

    fix vite config and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import dts from "vite-plugin-dts";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "index.ts"),
-      name: "ViteButton",
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      input:'src/main.tsx',
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
-  plugins: [react(), dts()],
+  plugins: [react()],
 });
