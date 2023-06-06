e65ea04cc5d956571a5dbae60ed81d86703c102e
abdelkader <bouzomitaabdelkader@gmail.com>
2023-06-06 10:49:05 +0100

    refactor: Removed deprecated code and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,7 @@ const App = (): JSX.Element => {
       )}
       <div className='w-screen flex flex-col items-center justify-start'>
         <h1 className='mt-8'>list-ninja</h1>
-        <a href='#' className='wisp'>
+        {/* <a href='#' className='wisp'>
           <svg
             fill='none'
             stroke-linecap='round'
@@ -165,7 +165,7 @@ const App = (): JSX.Element => {
           >
             <path d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'></path>
           </svg>
-        </a>
+        </a> */}
         <div className='max-w-[1200px] mx-auto flex justify-center items-stretch min-h-screen bg-gray-100'>
           {todoList.map((todo, index) => (
             <div
